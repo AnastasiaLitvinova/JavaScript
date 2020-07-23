723d01f20f597317ccce391c94690eed445d1b57
AnastasiaLitvinova <67413619+AnastasiaLitvinova@users.noreply.github.com>
2020-07-23 16:13:51 +0500

    Проверьте, исправила 6 задание

--- lesson-2/main6.js
+++ lesson-2/main6.js
@@ -3,49 +3,39 @@
 // В зависимости от переданного значения выполнить одну из арифметических операций (использовать функции из пункта 5) 
 // и вернуть полученное значение (применить switch).
 
-let arg1 = +prompt(), arg2 = +prompt()
-let divide, plus, multiply, minus // '/', '+', '*', '-'
+let arg1 = +prompt('Enter arg1: '), arg2 = +prompt('Enter arg1: ')
+const operation = prompt('Enter +, -, * or /: ')
 
-console.log(arg1, arg2)
-
-function mathDivide(arg1, arg2, divide) { // делит значения
-	if (arg1 < 0 && arg2 < 0) { // если значения отрицательные
-		return -arg1 > -arg2 ? arg1 / arg2 : arg2 / arg1
-	} else if (arg1 >= 0 && arg2 < 0) { // если одно значение отрицательно
-		return arg1 > -arg2 ? arg1 / arg2 : arg2 / arg1
-	} else if (arg1 < 0 && arg2 >= 0) { // если другое значение отрицательно
-		return -arg1 > arg2 ? arg1 / arg2 : arg2 / arg1
-	} else if (arg1 >= 0  && arg2 >= 0) { // если значения положительные
-		return arg1 > arg2 ? arg1 / arg2 : arg2 / arg1
-	}
-}
-function mathPlus(arg1, arg2, plus) {
+function plus(arg1, arg2) {
 	return arg1 + arg2
 }
-function mathMultiply(arg1, arg2, multiply) {
+function minus(arg1, arg2) {
+	return arg1 - arg2
+}
+function multiply(arg1, arg2) {
 	return arg1 * arg2
 }
-function mathMinus(arg1, arg2, minus) { // вычитает из большего меньшее
-	return arg1 > arg2 ? arg1 - arg2 : arg2 - arg1
+function divide(arg1, arg2) {
+	return arg1 / arg2
 }
 
-if ((arg1 && arg2 != 0) && ((arg1 % arg2 === 0) || (arg2 % arg1 === 0))) { // значения не равны 0 и делятся без остатка
-	divide = mathDivide(arg1, arg2, divide)
-	console.log('/') // выводит какая операция была произведена
-	console.log(divide)
+function mathOperation(arg1, arg2, operation) {
+	switch (true) {
+		case (operation == '+'):
+				return plus(arg1, arg2)
+				break
+		case (operation == '-'):
+				return minus(arg1, arg2)
+				break
+		case (operation == '*'):
+				return multiply(arg1, arg2)
+				break
+		case (operation == '/'):
+				return divide(arg1, arg2)
+				break
+	}
+
+	return operation
 }
-if ((arg1 < 0 && arg2 >= 0) || (arg1 >= 0 && arg2 < 0)) { // значения имеют разные знаки
-	plus = mathPlus(arg1, arg2, plus)
-	console.log('+')
-	console.log(plus)
-} else if (arg1 < 0 && arg2 < 0){ // если значения отрицательные
-	multiply = mathMultiply(arg1, arg2, multiply)
-	console.log('*')
-	console.log(multiply)
-} else if (arg1 >= 0  && arg2 >= 0) { // если значения положительные
-	minus = mathMinus(arg1, arg2, minus)
-	console.log('-')
-	console.log(minus)
-}  else {
-	console.log('Заданные значения не подходят условиям')
-}
+
+console.log(mathOperation(arg1, arg2, operation))
